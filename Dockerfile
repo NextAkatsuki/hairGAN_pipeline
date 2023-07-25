9e0b857a57d5e7eb7fbf0526cbe1a6ee662f97fa
oldentomato <ender1412@naver.com>
2023-07-25 22:51:31 +0900

    fly deploy

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,6 @@ RUN apt-get update && \
 
 RUN pip install -r requirements.txt
 
+EXPOSE 8000
 CMD uvicorn api.api:app --host=0.0.0.0 --port 8000
 
--- docker_run.sh
+++ docker_run.sh
@@ -2,4 +2,4 @@ docker build --tag hairgan_tensor:1.0 .
 
 docker run --name hairgan_tensor -it -d -p 8000:8000 hairgan_tensor:1.0
 
-docker exec -it hairgan_tensor /bin/bash
+docker logs hairgan_tensor
